98868a0dd7a666e372d3a84ca983db12fb7f7c77
John19110 <113050863+John19110@users.noreply.github.com>
2023-12-11 21:37:51 +0200

    Update Cooach.java

--- Cooach.java
+++ Cooach.java
@@ -1,44 +1,143 @@
-// package gym;
-// import java.util.Scanner;
+package gym;
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
 
-// class InvalidRangeException extends Exception
-//  {
-    
-//     public InvalidRangeException(String message) 
-//     {
-//         super(message);
-//     }
-// }
-// public class Coach extends Person 
+class Cooach extends Person 
+{
+    protected int WorkingHours;
+    protected int MaxCustomers;
+    protected List<Customer> customers;
+    protected List<Cooach> coaches;
 
-// {
-//     int working_Hours_Per_Day;
+    public Cooach(int iD, String PhoneNumber, String name, String address, String gender, String email, int workinghours) {
+        super(iD, PhoneNumber, name, address, gender, email);
+        WorkingHours=workinghours;
+        this. customers =new ArrayList<>();
+        this. coaches =new ArrayList<>();
+    }
+    public int getWorkingHours() {
 
-//     public Coach(int iD, int phoneNumber, String name, String address, String gender, String email,
-//             int working_Hours_Per_Day) 
-//       {
-//         super(iD, phoneNumber, name, address, gender, email);
-//         this.working_Hours_Per_Day = working_Hours_Per_Day;
-        
-    
-        
-//     }
-//         try
-//         {
-//             System.out.println("Enter number of working hours (MAX : 10)");
-//             Scanner input=new Scanner(System.in);
-//             working_Hours_Per_Day=input.nextInt();
-//         }
-            
-//         catch(InvalidRangeException e )
-        
-//         {
-//             System.out.println(e.getMessage());}
-        
-        
-//             private void checkRange(int working_Hours) throws InvalidRangeException {
-//                 if (working_Hours > 10 || working_Hours < 1) {
-//                     throw new InvalidRangeException("Renter the number of working hours (between 1 : 10)");
-//                 }
-//             }
-// }
+        return WorkingHours;
+    }
+
+    public void setWorkingHours(int workingHours) {
+
+        WorkingHours = workingHours;
+    }
+    public List<Customer> getCustomers() {
+        return customers;
+
+    }
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int iD) {
+        ID = iD;
+    }
+
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+
+    public void setPhoneNumber(String PhoneNumber) {
+        this.PhoneNumber = PhoneNumber;
+    }
+
+
+    public String getName() {
+        return Name;
+    }
+
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+
+    public String getAddress() {
+        return Address;
+    }
+
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+
+    public String getGender() {
+        return Gender;
+    }
+
+
+    public void setGender(String gender) {
+        Gender = gender;
+    }
+
+
+    public String getEmail() {
+        return Email;
+    }
+
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public int getMaxCustomers() {
+        return MaxCustomers;
+    }
+
+    public void setMaxCustomers(int maxCustomers) {
+        MaxCustomers = maxCustomers;
+    }
+
+    public void ListAllCustomers(int CoachId) {
+            System.out.println("Coach " + this.getName() + "Has customers : ");
+            for (Customer customer : customers) {
+                        if(CoachId==customer.getAssignedCoach().getID())
+                        {
+                    System.out.println("Customer ID: " + customer.getID());
+                    System.out.println("Customer Name: " + customer.getName());
+                    System.out.println("Customer Gender: " + customer.getGender());
+                    System.out.println("Customer Address: " + customer.getAddress());
+                    System.out.println("Customer Phone Number: " + customer.getPhoneNumber());
+                    System.out.println("Customer Email: " + customer.getEmail());
+                }
+            }
+        }
+    public List<InBody> getInBodyHistory(int CustomerId) 
+    {
+        for (Customer customer : customers) 
+        {
+            if(CustomerId==customer.getID())
+            {
+                return customer.getInBodies();
+            }
+
+        }
+        return null;
+    }
+    public Customer getCustomerByName(String customerName) {
+        for (Customer customer : customers) {
+            if (customer.getName().equals(customerName)) {
+                return customer;
+            }
+        }
+        return null;
+    }
+
+    public List<Customer> getCustomersByGender(String targetGender) 
+    {
+        List<Customer> result = new ArrayList<>();
+        for (Customer customer : customers) {
+            if (customer.getGender().equalsIgnoreCase(targetGender)) {
+                result.add(customer);
+            }
+        }
+        return result;
+    }
+}
