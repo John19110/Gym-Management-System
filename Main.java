70df2280103773a3fd0e97b0d10950797f711655
John19110 <113050863+John19110@users.noreply.github.com>
2023-12-13 23:39:50 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,120 @@
-// package gym;
-// import java.io.*;
+package Mygym;
 
-// public class Main {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
 
-//     public static void main(String[] args) 
-    
-//     {
-//         // Admin A=new Admin();
-//         // Gym G= new Gym(null,null,null);
-//         // Coach C=new Coach(0, 0, "null", null, null, null, 0)
-//         //    A.displayCustomersByCoach(G,C);
-//     }
+public class Main 
+{
+    Gym gym = new Gym(null, null, null);  
+    Admin admin = new Admin(gym);
+    public static void main(String[] args) throws FileNotFoundException, NullPointerException
+     {
+        Main main=new Main();
 
-// }
+       main.read("inputdata.txt");
+    //    main.write("outputdata.txt");
+
+    }
+
+
+    public  void read(String filepath) throws FileNotFoundException
+     {
+
+        File file = new File(filepath);
+        Scanner inputfile;
+
+        if (file.exists()) {
+            inputfile = new Scanner(file);
+
+            for (int i = 0; i < 2 && inputfile.hasNextLine(); i++) {
+                try {
+                    int CustomerId = Integer.parseInt(inputfile.nextLine());
+                    String Customername = inputfile.nextLine();
+                    String Customeraddress = inputfile.nextLine();
+                    String Customergender = inputfile.nextLine();
+                    String Customeremail = inputfile.nextLine();
+                    String Customerphonenumber = inputfile.nextLine();
+
+                    int CoachId = Integer.parseInt(inputfile.nextLine());
+                    String Coachname = inputfile.nextLine();
+                    String Coachaddress = inputfile.nextLine();
+                    String Coachgender = inputfile.nextLine();
+                    String Coachemail = inputfile.nextLine();
+                    String Coachphonenumber = inputfile.nextLine();
+
+                    String stringstartdate = inputfile.nextLine();
+                    String stringenddate = inputfile.nextLine();
+                    String monthlyPlan = inputfile.nextLine();
+                    int numMonthsRegistered = Integer.parseInt(inputfile.nextLine());
+                    int pric = Integer.parseInt(inputfile.nextLine());
+                    String stringdateofsubescription = inputfile.nextLine();
+                    String stringdateofinbody = inputfile.nextLine();
+                    int height = Integer.parseInt(inputfile.nextLine());
+                    int totalWeight = Integer.parseInt(inputfile.nextLine());
+                    int body_Fat_Mass = Integer.parseInt(inputfile.nextLine());
+                    int minerals = Integer.parseInt(inputfile.nextLine());
+                    int total_Body_Water = Integer.parseInt(inputfile.nextLine());
+                    int protein = Integer.parseInt(inputfile.nextLine());
+                    int workinghours = 0; // Default value, since it's not provided in the sample data
+                    // Parse non-numeric values using try-catch
+                    try {
+                        workinghours = Integer.parseInt(inputfile.nextLine());
+                    } catch (NumberFormatException e) {
+                        // Handle the exception, e.g., log the error
+                        e.printStackTrace();
+                    }
+
+                    String equipmentname = inputfile.nextLine();
+                    int Equipmentquantity = 0; // Default value
+                    int equipmentcode = 0; // Default value
+
+                    // Parse non-numeric values using try-catch
+                    try {
+                        Equipmentquantity = Integer.parseInt(inputfile.nextLine());
+                        equipmentcode = Integer.parseInt(inputfile.nextLine());
+                    } catch (NumberFormatException e) {
+                        // Handle the exception, e.g., log the error
+                        e.printStackTrace();
+                    }
+                    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                    Date startdate = dateFormat.parse(stringstartdate);
+                    Date enddate = dateFormat.parse(stringenddate);
+                    Date dateofsubescribtion = dateFormat.parse(stringdateofsubescription);
+                    Date dateofinbody = dateFormat.parse(stringdateofinbody);
+
+                    Membership membership = new Membership(startdate, enddate, monthlyPlan, numMonthsRegistered, pric);
+                    InBody inbody = new InBody(CustomerId, Customername, Customeraddress, Customergender, Customeremail, Customerphonenumber,
+                            dateofinbody, height, totalWeight, body_Fat_Mass, minerals, total_Body_Water, protein);
+                    Subscription subscription = new Subscription(CustomerId, CoachId, membership, dateofsubescribtion);
+                    Coach coach = new Coach(CoachId, Coachname, Coachaddress, Coachgender,
+                            Coachemail, Coachphonenumber, workinghours, null);
+                    Customer customer = new Customer(CustomerId, Customername, Customeraddress, Customergender, Customeremail,
+                            Customerphonenumber, subscription, null, coach, membership);
+                    Equipment equipment = new Equipment(equipmentname, equipmentcode, Equipmentquantity);
+
+                    admin.addCustomer(customer);
+                    admin.addCoach(coach);
+                    admin.addEquipment(equipment);
+
+                } catch (NumberFormatException | ParseException e) {
+                    // Handle the exception (e.g., log the error, provide a user-friendly message)
+                    e.printStackTrace();
+                }
+            }
+
+          
+
+            inputfile.close(); // Close the scanner after reading the file
+        }
+    }
+        public  void write(String filepath)
+        {
+
+        }
+
+}
+
